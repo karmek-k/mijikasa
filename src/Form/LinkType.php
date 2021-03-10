7c4bb9afa518478b3a485a75238f32ae5564665e
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-03-10 17:07:12 +0100

    Change UrlType to TextType (in order to validate)

--- src/Form/LinkType.php
+++ src/Form/LinkType.php
@@ -5,7 +5,7 @@ namespace App\Form;
 use App\Entity\Link;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,7 +14,7 @@ class LinkType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('url', UrlType::class, [
+            ->add('url', TextType::class, [
                 'label' => 'URL to be shortened',
             ])
             ->add('submit', SubmitType::class, [
