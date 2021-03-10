e3288d748ffc1495e98a0c6b3ff51e3ec10ee7fd
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-03-10 17:00:52 +0100

    Add validator

--- src/Entity/Link.php
+++ src/Entity/Link.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\LinkRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=LinkRepository::class)
@@ -20,11 +21,14 @@ class Link
     /**
      * @ORM\Column(type="text")
      */
+    #[Assert\NotBlank]
+    #[Assert\Url]
     private $url;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
+    #[Assert\Blank]
     private $hash;
 
     public function getId(): ?int
