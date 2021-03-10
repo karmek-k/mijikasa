28fe572891e1c28fa61559ca53e44db4e7280d85
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-03-10 16:31:39 +0100

    Create a basic home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,6 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
     }
 }
