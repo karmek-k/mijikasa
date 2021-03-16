e011518913c2e1d67329c9be4dc6ea1f5834b8eb
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-03-16 15:57:16 +0100

    Restore setHash setter and delete the constructor in Link.php

--- src/Entity/Link.php
+++ src/Entity/Link.php
@@ -25,11 +25,6 @@ class Link
      */
     private $hash;
 
-    public function __construct(string $hash)
-    {
-        $this->hash = $hash;
-    }
-
     public function getUrl(): ?string
     {
         return $this->url;
@@ -45,5 +40,12 @@ class Link
     public function getHash(): ?string
     {
         return $this->hash;
+    }
+
+    public function setHash(string $hash): self
+    {
+        $this->hash = $hash;
+
+        return $this;
     }
 }
