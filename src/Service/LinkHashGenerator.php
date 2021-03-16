fcb835f971ff2d3eda7410bb3552663cbe5c4c0f
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-03-16 16:11:49 +0100

    Create LinkHashGenerator.php, delete LinkFactory.php

---
+++ src/Service/LinkHashGenerator.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Service;
+
+
+use App\Entity\Link;
+use App\Repository\LinkRepository;
+
+class LinkHashGenerator
+{
+    public function __construct(
+        private RandomStringGenerator $stringGenerator,
+        private HashAvailabilityChecker $checker,
+    ) {}
+
+    public function generate(int $minHashLength = 5): string
+    {
+        do {
+            // creates another hash if the generated one is not available
+            $hash = $this->stringGenerator->generate($minHashLength);
+            ++$minHashLength;
+        } while (!$this->checker->checkAvailability($hash));
+
+        return $hash;
+    }
+}
